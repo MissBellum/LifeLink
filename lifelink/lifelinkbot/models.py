01db709bb6711edb3ce494c0accd7c0551405d72
Missbellum <enjaykanz@gmail.com>
2024-11-04 21:41:43 +0100

    Ran succesful Gemini api test + Created models

--- lifelink/lifelinkbot/models.py
+++ lifelink/lifelinkbot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class UserMessage(models.Model):
+    message = models.TextField(max_length=800)
+    created_at = models.TimeField(auto_now_add=True)
+    
+class BotResponse(models.Model):
+    response = models.TextField()
--- lifelink/lifelinkbot/views.py
+++ lifelink/lifelinkbot/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from .models import UserMessage, BotResponse
 
+import os
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
 # Create your views here.
+load_dotenv()
+
+gen_ai.configure(api_key = os.getenv('API_KEY'))
+
+model = gen_ai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Write a story about a magic backpack.")
+print(response.text)
+# def send_message(request):
+#     if request.method == 'POST':
+#         message = request.POST.get
