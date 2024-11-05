26025e690525f4f1b899a488caa899558ee09f26
Missbellum <enjaykanz@gmail.com>
2024-11-05 18:03:46 +0100

    Added home route

--- lifelink/lifelinkbot/views.py
+++ lifelink/lifelinkbot/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# from .models import UserMessage, BotResponse
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import UserMessage, BotResponse
 
 import os
 from dotenv import load_dotenv
@@ -10,9 +10,20 @@ load_dotenv()
 
 gen_ai.configure(api_key = os.getenv('API_KEY'))
 
-model = gen_ai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
-# def send_message(request):
-#     if request.method == 'POST':
-#         message = request.POST.get
+def generate(prompt):
+    model = gen_ai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+    return prompt
+
+# print(response.text)
+
+def home(request):
+    return render(request, 'home.html')
+
+def get_help(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt')
+        user_prompt = UserMessage.objects.create(prompt=prompt)
+        response = generate(prompt)
+        return JsonResponse({'response': response})
+    
