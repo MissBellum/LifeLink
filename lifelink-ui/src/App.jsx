285b23f3119ee896457ddc014be6b4ad56b937a5
Missbellum <enjaykanz@gmail.com>
2024-11-06 19:19:21 +0100

    Removed main.jsx as index.jsx is present

--- lifelink-ui/src/App.jsx
+++ lifelink-ui/src/App.jsx
@@ -19,7 +19,7 @@ function App({ options }) {
             </div>
             
             <section>
-              <Options options={options} />
+              <Options />
             </section>
 
             <section>
--- lifelink-ui/src/index.js
+++ lifelink-ui/src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Options from './components/Options';
 import reportWebVitals from './reportWebVitals';
+
+const OPTIONSDATA = [
+  { id: 1, name: "Police Assistance", description: "Contact police for emergencies." },
+  { id: 2, name: "Medical Emergency", description: "Get immediate medical help." },
+  { id: 3, name: "Fire Department", description: "Call fire services for emergencies." },
+  { id: 4, name: "Roadside Assistance", description: "Request roadside help for vehicle issues." },
+  { id: 5, name: "Mental Health Support", description: "Get support for mental health crises." },
+  { id: 6, name: "Report Suspicious Activity", description: "Report suspicious activities to authorities." },
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Options options={ OPTIONSDATA } />
   </React.StrictMode>
 );
 
