fb815d0beda28af5157b2ee03328b07485adb8a0
Missbellum <enjaykanz@gmail.com>
2024-11-06 19:03:29 +0100

    Created OPTIONSDATA and populated same object with related data

--- lifelink-ui/src/App.jsx
+++ lifelink-ui/src/App.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import InputForm from "./components/InputForm";
+import Options from "./components/Options";
 
-
-function App(props) {
+function App({ options }) {
+  
   return (
     <div className="app">
       <div className="app__header">
@@ -10,18 +12,22 @@ function App(props) {
         </header>
       </div>
 
-      <div>
-        <main>
+      <main>
+        <div>
             <div className="home__content">
               <h2>Welcome to LifeLink. How can I help?</h2>
-              
             </div>
             
-            <div>
+            <section>
+              <Options options={options} />
+            </section>
+
+            <section>
               <InputForm />
-            </div>
-        </main>
-      </div>
+            </section>
+        </div>
+      </main>
+
 
     </div>
 
--- lifelink-ui/src/components/Options.jsx
+++ lifelink-ui/src/components/Options.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+
 function Options(props) {
+    const helpList = props.options?.map((option) => (
+        <div id={ option.id } key={ option.id } className="option__item">
+            <h4>{ option.name }</h4>
+            <p>{ option.description }</p>
+        </div>
+  ));
+
     return (
-        <div>
-            
+        <div className="option__div">
+            { helpList }
         </div>
     )
 }
