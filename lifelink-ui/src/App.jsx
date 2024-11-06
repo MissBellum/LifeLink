b537b8f3757a2aec8a53d4e491f8967fc7d2fb26
Missbellum <enjaykanz@gmail.com>
2024-11-06 12:54:12 +0100

    Created InputForm component and added styling

--- lifelink-ui/src/App.jsx
+++ lifelink-ui/src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css';
+import InputForm from "./components/InputForm";
+
 
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        LifeLink
-      </header>
+    <div className="app">
+      <div>
+        <header className="app-header">
+          <h1>Welcome to LifeLink. How can I help?</h1>
+        </header>
+      </div>
+      
+      <div>
+        <InputForm />
+      </div>
     </div>
+
   );
 }
 
--- lifelink-ui/src/components/InputForm.jsx
+++ lifelink-ui/src/components/InputForm.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 function InputForm() {
     return (
-        <div className="prompt-div">
-            <input id="prompt" className="promptInput" type="text" placeholder="Message" />
-            <div>
-                <button className='prompt-submit' type='button'>
-                <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
-                </button>
+        <div>
+            <div className="prompt-div">
+                <input 
+                    id="prompt" 
+                    className="prompt-input" 
+                    type="text" 
+                    placeholder="Message" 
+                    cols="20" 
+                    rows="3" 
+                />
+                <div>
+                    {/* <input className='prompt-submit' type='button' /> */}
+                    <FontAwesomeIcon className='submit-icon' size='2x' icon={ faArrowRight } />
+                </div>
             </div>
+            
         </div>
     )
 }
