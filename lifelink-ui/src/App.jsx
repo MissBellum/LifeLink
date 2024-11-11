dbcd168831f117b8bfecf3f5bf8f721b391dd9da
Missbellum <enjaykanz@gmail.com>
2024-11-11 23:30:27 +0100

    Changed header styling to CSS file

--- lifelink-ui/src/App.jsx
+++ lifelink-ui/src/App.jsx
@@ -5,15 +5,14 @@ import Options from "./components/Options";
 function App({ options }) {
   return (
     <div className="app bg-white min-h-screen flex flex-col overflow-hidden">
-      {/* Updated header with the orange color */}
-      <div className="app__header" style={{ backgroundColor: '#ff6200' }}>
-  <header className="header__text text-center text-3xl font-extrabold text-white py-3 px-6 relative">
-    <span className="absolute inset-0 bg-gradient-to-r from-[#ff6200] to-[#e65500] opacity-30 rounded-full"></span>
-    <h1 className="relative z-10 text-white tracking-wide text-shadow-md">
-      LifeLink
-    </h1>
-  </header>
-</div>
+      <div className="app__header">
+        <header className="header__text text-center text-3xl font-extrabold text-white py-3 px-6 relative">
+          <span className="absolute inset-0 bg-gradient-to-r from-[#ff6200] to-[#e65500] opacity-30 rounded-full"></span>
+          <h1 className="relative z-10 text-white tracking-wide text-shadow-md">
+            LifeLink
+          </h1>
+        </header>
+      </div>
 
 
       <main className="flex-grow py-4 px-4 overflow-y-auto">
