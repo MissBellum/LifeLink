e1847d06f467c9d250ef34aef16748ed6a94b8df
Missbellum <enjaykanz@gmail.com>
2024-11-16 00:00:13 +0100

    Started JS code for Option component

---
+++ lifelink-ui/src/app.js
@@ -0,0 +1,13 @@
+document.getElementById('options-dropdown').addEventListener('change', function () {
+    const messageBox = document.getElementById('message-box');
+    const selectedOptionText = this.options[this.selectedIndex].text;
+  
+    messageBox.value = selectedOptionText;
+  
+    sendMessage(selectedOptionText);
+  });
+  
+  function sendMessage(message) {
+    console.log(`Message sent: ${message}`);
+  }
+  
