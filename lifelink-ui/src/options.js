dcf51099f36d85542f9b4e53acddb652dde2e8cd
Missbellum <enjaykanz@gmail.com>
2024-11-16 22:14:48 +0100

    Changed function to anonymous

--- lifelink-ui/src/options.js
+++ lifelink-ui/src/options.js
@@ -1,4 +1,4 @@
-document.getElementById('options-dropdown').addEventListener('change', function () {
+document.getElementById('options-dropdown').addEventListener('change', () => {
     const messageBox = document.getElementById('message-box');
     const selectedOptionText = this.options[this.selectedIndex].text;
   
