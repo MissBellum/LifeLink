f77716cbf2c2214e427890ade036034e69d2cca3
Missbellum <enjaykanz@gmail.com>
2024-11-26 19:28:50 +0100

    Created test for Options.jsx component using Jest

--- lifelink-ui/src/setupTests.js
+++ lifelink-ui/src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { render, screen, expect } from "@testing-library/react"
+import Options from './components/Options';
+
+test('renders emergency options', () => {
+    render(<Options />);
+    expect(screen.getByText('Contact Police').toBeInTheDocument());
+});
