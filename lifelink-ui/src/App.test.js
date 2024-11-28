1fdeb9c7dc8cb13ef1db63512a2259a3b296ab69
Missbellum <enjaykanz@gmail.com>
2024-11-28 20:57:29 +0100

    Modified test to render App instead of Option component

--- lifelink-ui/src/App.test.js
+++ lifelink-ui/src/App.test.js
@@ -1,9 +1,9 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-// import Options from './components/Options';
 
 test('renders emergency options', () => {
   render(<App />);
-  expect(screen.getByText('Contact Police')).toBeInTheDocument();
+  expect(screen.getByText('Contact police')).toBeInTheDocument();
   expect(screen.getByText('Call Ambulance')).toBeVisible(); // test if fail
 });
