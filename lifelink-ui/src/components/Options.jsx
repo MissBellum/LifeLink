3e10103efb325229b19a8736024a883aac11a535
Missbellum <enjaykanz@gmail.com>
2024-12-02 08:40:11 +0100

    Added form element to InputForm.jsx

--- lifelink-ui/src/components/Options.jsx
+++ lifelink-ui/src/components/Options.jsx
@@ -5,16 +5,15 @@ function Options(props) {
     <div
       id={option.id}
       key={option.id}
-      className="options__item bg-white rounded-lg shadow-lg p-4 transform hover:scale-105 transition-transform duration-300 ease-out hover:shadow-2xl mb-2"
-    >
+      className="options__item bg-white rounded-lg shadow-lg p-4 transform hover:scale-105 transition-transform duration-300 ease-out hover:shadow-2xl mb-2">
       <h4 className="text-lg font-semibold text-gray-800 mb-2">{option.name}</h4>
-      <p href="#" className="options__text font-sans text-gray-600 text-sm">{option.description}</p>
+      <p className="options__text font-sans text-gray-600 text-sm">{option.description}</p>
     </div>
   ));
 
   return (
     <div className="options__div grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-2">
-      {helpList}
+      { helpList }
     </div>
   );
 }
