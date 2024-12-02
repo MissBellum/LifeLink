70a69544872525225fe0ef51368b1c01bd6290ca
Missbellum <enjaykanz@gmail.com>
2024-12-02 09:43:15 +0100

    Resized Options display to fit two items

--- lifelink-ui/src/components/Options.jsx
+++ lifelink-ui/src/components/Options.jsx
@@ -5,14 +5,14 @@ function Options(props) {
     <div
       id={option.id}
       key={option.id}
-      className="options__item bg-white rounded-lg shadow-lg p-4 transform hover:scale-105 transition-transform duration-300 ease-out hover:shadow-2xl mb-2">
+      className="options__item bg-white rounded-lg shadow-lg p-3 transform hover:scale-105 transition-transform duration-300 ease-out hover:shadow-2xl mb-2">
       <h4 className="text-lg font-semibold text-gray-800 mb-2">{option.name}</h4>
       <p className="options__text font-sans text-gray-600 text-sm">{option.description}</p>
     </div>
   ));
 
   return (
-    <div className="options__div grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-2">
+    <div className="options__div grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4 p-2">
       { helpList }
     </div>
   );
