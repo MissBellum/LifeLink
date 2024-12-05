24dbb7cc82aaa80c49e9c6e80be1c5b3bf9fb310
Missbellum <enjaykanz@gmail.com>
2024-12-05 19:33:35 +0100

    test

--- lifelink-ui/src/components/InputForm.jsx
+++ lifelink-ui/src/components/InputForm.jsx
@@ -3,27 +3,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function InputForm() {
-  
   return (
+    <div className="flex justify-center items-center w-full">
       <form method="POST" action="" className="w-full max-w-2xl">
-        <div className="home__prompt__div flex items-center bg-white rounded-full shadow-lg p-3 w-full">
+        <div className="home__prompt__div flex items-center bg-white rounded-full shadow-lg p-3 mt-10 w-full">
           <input
             id="home-prompt"
             className="home__prompt flex-grow rounded-l-full px-6 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#ff6200] placeholder-gray-400"
             type="text"
             placeholder="Message"
-            required
           />
           <div className="pr-5">
             <button
               type="submit"
               className="home__submit__icon bg-[#ff6200] hover:bg-[#e65500] text-white w-12 h-10 rounded-full ml-2 transition duration-300 ease-in-out transform hover:scale-105"
             >
-              <FontAwesomeIcon size="2xl" icon={faArrowRight} />
+              <FontAwesomeIcon size="xl" icon={faArrowRight} />
             </button>
           </div>
         </div>
       </form>
+    </div>
   );
 }
 
